c377393401ee5aa93caeff9fe99c1881200c6c19
PulkitAsri <pulkit.asri@gmail.com>
2023-06-20 03:34:05 +0530

    Refactor

--- app.js
+++ app.js
@@ -1,49 +1,41 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
 const bodyparser = require('body-parser');
 const cookieparser = require('cookie-parser');
-const cors = require("cors");
-const dotenv = require("dotenv");
-dotenv.config()
-const { Client } = require("@googlemaps/google-maps-services-js");
-const swaggerUI = require("swagger-ui-express");
-const YAML = require("yamljs");
+const cors = require('cors');
+const dotenv = require('dotenv');
+// const { Client } = require('@googlemaps/google-maps-services-js');
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
 const swaggerDocument = YAML.load('./swagger.yaml');
 
+const apiRoutes = require('./Routes/routes');
 
-const apiRoutes = require("./Routes/routes");
+const app = express();
 
-// import cookieparser from "cookie-parser";
-// import cors from "cors";
-//import swaggerUI from "swagger-ui-express";
-//import YAML from 'yamljs';
-//import dotenv from "dotenv" 
-// import authRoutes from "./Routes/authentication.js";
-// import userRoutes from "./Routes/user.js";
-// import allusersRoutes from "./Routes/allusersRoutes.js";
-//const specs = swaggerJsDoc(options);
-//Middleware
+dotenv.config();
 
-//PORT
+// Middleware
+app.use(bodyparser.json());
+app.use(cookieparser());
+app.use(cors());
 
+// MongoDB connection
+mongoose
+    .connect(process.env.DATABASE_URI)
+    .then(() => console.log('DB connected'))
+    .catch(error => {
+        console.error('Error connecting to MongoDB:', error);
+        process.exit(1);
+    });
 
-// MongoDb connection
-var db=mongoose.connect(process.env.DATABASE_URI).then(console.log("DB connected"))
-//.catch(error => console.log(error));
-
-//Middleware
-app.use(bodyparser.json())
-app.use(cookieparser())
-app.use(cors())
-
-//Routes
-app.use("/api", apiRoutes);
+// Routes
+app.use('/api', apiRoutes);
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
-app.listen(process.env.PORT || 8000, () => {
-    console.log(`Listening on a port ${process.env.PORT || 8000}`);
-})
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
 
 module.exports = app;
-// MongoDb connection
